dfae99e39e9789bf7401d66230eb96cf47607cb7
Wade Benson <wbenson@apple.com>
2015-09-04 22:52:41 -0700

    update pkg

--- pkg.sh
+++ pkg.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PKG_PATH=/tmp/siteaccess.out
+PKG_NAME=siteaccess
+PKG_PATH=/tmp/${PKG_NAME}.out
 VER_MAJOR=2
 VER_MINOR=4
 VER_REV=$(cat version.txt)
@@ -9,9 +10,9 @@ if [[ -d "${PKG_PATH}" ]]; then
     rm -rf "${PKG_PATH}"
 fi
 mkdir -p ${PKG_PATH}
-git archive --format=tar --prefix="jssor"/ HEAD | tar Cxf "${PKG_PATH}" -
+git archive --format=tar --prefix="${PKG_NAME}"/ HEAD | tar Cxf "${PKG_PATH}" -
 ((VER_REV++))
-sed -i "" -e "s/VER_MAJOR/${VER_MAJOR}/g;s/VER_MINOR/${VER_MINOR}/g;s/VER_REV/${VER_REV}/g" ${PKG_PATH}/jssor/manifest.xml
+sed -i "" -e "s/VER_MAJOR/${VER_MAJOR}/g;s/VER_MINOR/${VER_MINOR}/g;s/VER_REV/${VER_REV}/g" ${PKG_PATH}/${PKG_NAME}/manifest.xml
 echo "${VER_REV}" > version.txt
 git add version.txt
 git commit -m "pkg tag ${VER_MAJOR}.${VER_MINOR}.${VER_REV}"
@@ -20,4 +21,4 @@ git push --all
 git push --tags
 
 cd "${PKG_PATH}"
-zip -r "/tmp/jssor-${VER_MAJOR}.${VER_MINOR}.${VER_REV}.zip" jssor
+zip -r "/tmp/${PKG_NAME}-${VER_MAJOR}.${VER_MINOR}.${VER_REV}.zip" ${PKG_NAME}
