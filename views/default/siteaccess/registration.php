5e05e88a26aea10a6f291358f6364c750c3e433c
Wade Benson <wade@cryptogeek.org>
2015-09-04 23:45:53 -0700

    fix spacing

--- views/default/siteaccess/registration.php
+++ views/default/siteaccess/registration.php
@@ -2,11 +2,14 @@
     <?php
 	if (siteaccess_site_password_enabled())
 	    echo "<label>" . elgg_echo('siteaccess:sitepassword:text') . "<br />" . elgg_view('input/password' , array('name' => 'siteaccesspassword', 'value' => $siteaccesspassword, 'class' => "general-textarea")) . "</label><br />";
-	if (siteaccess_coppa_enabled())
-	    echo "<label><br />" . elgg_view('input/checkbox' , array('name' => 'coppa', 'value' => true, 'label' => elgg_echo('siteaccess:coppa:text'))) . "</label><br />";
+	if (siteaccess_coppa_enabled()) {
+		echo "<label><br />" . elgg_view('input/checkbox' , array('name' => 'coppa', 'value' => true, 'label' => elgg_echo('siteaccess:coppa:text'))) . "</label><br />";
+		echo "<br />";
+	}
 
-	if (siteaccess_reg_captcha_enabled())
-	    echo elgg_view('siteaccess/code', $vars);
+	if (siteaccess_reg_captcha_enabled()) {
+		echo elgg_view('siteaccess/code', $vars);
+	}
 
     ?>
 </div>
