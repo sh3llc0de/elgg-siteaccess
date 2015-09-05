f9ae59407f61e70bd0c4fdbd459fdb62963fc25b
Wade Benson <wbenson@apple.com>
2015-09-04 23:09:34 -0700

    update

--- siteaccess/views/default/siteaccess/login.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <?php
-	if (siteaccess_login_captcha_enabled() AND ($_SESSION['login_failure'] >= siteaccess_login_threshold()))
-	    echo elgg_view('siteaccess/code', $vars);
-    ?>
-</div>
